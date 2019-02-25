951a16f2213b29a3aff85a969e7b584d7290ecb0
PeterTing <a0917209079@gmail.com>
2019-02-25 23:06:07 +0800

    before eject

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Koa from 'koa';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method}${ctx.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async ctx => {
+    ReactDOM.render( < App / > , document.getElementById('root'));
+})
+
+app.listen(3000);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
