3fddbbb72ff2db3b7e7edd431fe7061c3c50fb58
EZou <jack98526@gmail.com>
2019-03-28 21:04:24 +0800

    - api implemented

--- src/web/api/index.js
+++ src/web/api/index.js
@@ -1,33 +1,111 @@
 import axios from 'axios'
+import JWT from 'jsonwebtoken'
 
-const baseURL = 'https://app.goodtogo.tw/manager'
+const baseURL = 'https://app.goodtogo.tw/dev'
 
-function randomHex(length=10){
-	let n = length, s = '';
-	while(n--){
-		s += (Math.random() * 16 | 0).toString(16)
-	}
-	return s
+const HeaderType = Object.freeze({
+    DEFAULT: 'DEFAULT',
+    USER_API_KEY: 'USER_API_KEY',
+    JWT_ADD_ORDER_TIME: 'JWT_ADD_ORDER_TIME',
+    JWT: 'JWT',
+    API_KEY: 'API_KEY',
+    JSON_CONTENT: 'JSON_CONTENT',
+    NO_CACHE: 'NO_CACHE'
+})
+
+const uri = Object.freeze({
+    login: '/users/login',
+    containerList: '/containers/get/list',
+    storeList: '/stores/list',
+    createDeliveryList: '/deliveryList/create/'
+})
+
+const jwtFactory = {
+    standard: (secretKey) => 
+        JWT.sign({
+            jti: 'manager',
+            iat: Date.now(),
+            exp: new Date().setDate(new Date().getDate() + 3)
+        }, secretKey),
+    addOrderTime: secretKey =>
+        JWT.sign({
+            jti: 'manager',
+            iat: Date.now(),
+            exp: new Date().setDate(new Date().getDate() + 3),
+            orderTime: Date.now()
+        }, secretKey)
+}
+
+function requestConfigHeaderWrapper(...types) {
+    let config = {}
+    const admin = JSON.parse(localStorage.admin)
+    types.forEach(type => {
+        switch (type) {
+            case HeaderType.DEFAULT:
+                config['reqTime'] = String(new Date().getTime())
+                config['reqID'] = 'manager'
+                return
+            case HeaderType.USER_API_KEY:
+                config['userApiKey'] = ''
+                return
+            case HeaderType.JWT_ADD_ORDER_TIME:
+                config['Authorization'] = jwtFactory.addOrderTime(localStorage.admin.secretKey)
+                config['apiKey'] = admin.apiKey
+                return
+            case HeaderType.JWT:
+                config['Authorization'] = jwtFactory.standard(localStorage.admin.secretKey)
+            case HeaderType.API_KEY:
+                config['apiKey'] = admin.apiKey
+                return
+            case HeaderType.JSON_CONTENT:
+                config['Content-Type'] = 'application/json'
+                return
+            case HeaderType.NO_CACHE:
+                config['Cache-Control'] = 'no-cache'
+        }
+    })
+    return config
 }
 
 function login(phone, password) {
-    const body = JSON.stringify({
+    const body = {
         phone, password
-    })
-
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            'repTime': String(new Date().getTime()),
-            'reqID': randomHex()
-        }
     }
 
-    return axios.post(baseURL+'/login', body, config)
+    const config = {
+        headers: requestConfigHeaderWrapper(HeaderType.DEFAULT, HeaderType.JSON_CONTENT)
+    }
+
+    return axios.post(baseURL+'/users/login', body, config)
+        .then((response) => JWT.decode(response.headers.authorization))
+}
+
+function getContainerList() {
+    const config = {
+        headers: requestConfigHeaderWrapper(HeaderType.DEFAULT)
+    }
+    return axios.get(baseURL+uri.containerList, config)
+        .then(res => res.data)
+}
+
+function getStoreList() {
+    const config = {
+        headers: requestConfigHeaderWrapper(HeaderType.DEFAULT)
+    }
+    return axios.get(baseURL+uri.storeList, config)
+        .then(res => res.data)
+}
+
+function createDeliveryList(storeId, box) {
+    const config = {
+        headers: requestConfigHeaderWrapper(HeaderType.JWT)
+    }
+    return axios.get(baseURL+uri.storeList, config)
+        .then(res => res.data)
 }
 
 const API = {
-    login
+    login, getContainerList, getStoreList
 }
 
 export default API
--- src/web/components/App/index.js
+++ src/web/components/App/index.js
@@ -36,11 +36,38 @@ const drawerWidth = 240
 const appBarHeight = 64
 
 API.login('0933356403', '')
-	.then((response) => {
-		console.log(response.status)
+	.then((response) => 
+		response.roles.admin ?
+			response.roles.admin :
+			Promise.reject(new Error('unauthorized'))
+	)
+	.then(admin => {
+		localStorage.auth = JSON.stringify(admin)
+	})
+	.then(API.getContainerList)
+	.then(list => {
+		const containers = JSON.stringify({
+			dict: list.containerDict,
+			type: list.containerType
+		})
+		localStorage.containers = containers
+	})
+	.then(API.getStoreList)
+	.then(list => 
+		list['shop_data']
+			.filter(store=>store.contract.borrowable)
+			.map(store=>({id: store.id, name: store.name}))
+	)
+	.then(list => {
+		localStorage.stores = JSON.stringify(list)
 	})
 	.catch((err) => {
-		console.log('err'+err)
+		switch (err.message) {
+			case 'unauthorized':
+				alert('沒有權限登入後台')
+			default:
+				alert(err)
+		}
 	})
 
 const App = (props) => {
